c7c6a57733dc28bbf1a83c816c95884ef08eed52
Roberto Baez <rbaezsp@gmail.com>
2022-05-18 17:35:54 -0300

    requisitos do 1 ao 5 completos

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,47 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(num1, num2) {
   // seu código aqui
+  let result = num1 && num2 === true ? true : false;
+
+  return result;
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+
+  const area = (base * height) / 2;
+
+  return area;
+
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(str) {
   // seu código aqui
+
+  const strSeparator = str.split(' ');
+
+  return strSeparator;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(arr) {
   // seu código aqui
+
+
+  return `${arr[arr.length - 1]}, ${arr[0]}`;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  const numbers = [];
+  numbers.push(wins, ties);
+
+  const calcWins = numbers.reduce((value) => value * 3);
+  const result = calcWins && ties === 0 ? 0 : calcWins + ties;
+  return result;
 }
 
 // Desafio 6
@@ -42,6 +63,7 @@ function fizzBuzz() {
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
